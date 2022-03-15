79793ac70eecc8e70d3f0da9ffff000c16787d75
TheForexCave <theforexcave@gmail.com>
2022-03-15 04:15:50 -0400

    Added default grid, grid reset button + logic

---
+++ app.js
@@ -0,0 +1,85 @@
+//Creates a reference for elements
+const container = document.querySelector('#container');
+container.classList.add('box')
+
+const heading = document.querySelector('h1');
+heading.style.display = 'flex';
+heading.style.flexDirection = 'column';
+heading.style.alignItems = 'center';
+
+const reset = document.createElement('button');
+reset.textContent = 'RESET';
+reset.style.display = 'block';
+
+heading.appendChild(reset);
+
+
+for (i=0; i < 256; i++){
+    const div1 = document.createElement('div');
+    div1.classList.add('gridBox')
+    
+    div1.addEventListener('mouseover', function(e){
+        e.target.style.backgroundColor = 'red';
+    })
+
+    div1.style.width = '65.5px';
+    div1.style.display = 'flex';
+    div1.style.height = '65.5px';
+    div1.style.border = 'solid black 1px'
+    div1.style.backgroundColor = 'gray';
+
+    container.appendChild(div1);
+}
+
+function generateGrid(){
+    var gridSize = window.prompt('Enter new grid size')
+    let area = gridSize * gridSize;
+    let boxSize = (1080/gridSize-2)+'px';
+
+    for (i=0; i < area; i++){
+        const div1 = document.createElement('div');
+        div1.classList.add('gridBox')
+        
+        div1.addEventListener('mouseover', function(e){
+            e.target.style.backgroundColor = 'red';
+        })
+    
+        div1.style.width = boxSize;
+        div1.style.display = 'flex';
+        div1.style.height = boxSize;
+        div1.style.border = 'solid black 1px'
+        div1.style.backgroundColor = 'gray';
+    
+    
+    
+        container.appendChild(div1);
+    }
+}
+
+function resetGrid(){
+    const box = document.querySelectorAll('.gridBox');
+
+    box.forEach(function(e) {
+        // e.style.backgroundColor = 'grey';
+        e.style.display = 'none';
+    })
+
+    generateGrid();
+    
+}
+
+reset.addEventListener('click', () => {
+    resetGrid();
+})
+
+
+
+
+
+
+
+
+
+
+
+
